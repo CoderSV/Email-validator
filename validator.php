a8a65874f1360039ff6eda9c228e7e7ed22488ab
CoderSV <CoderSV@users.noreply.github.com>
2015-06-01 17:29:26 -0600

    Create validator.php

---
+++ validator.php
@@ -0,0 +1,39 @@
+<?php
+
+
+/**
+ * Example 1
+ * Validate a single Email via SMTP
+ */
+
+if($_POST['email'] != ''){
+// the email to validate  
+$email = $_POST['email'];  
+
+require('smtp-validate-email.php');
+
+$sender  = 'info@svgamestore.com'; // for SMTP FROM:<> command
+
+
+// do the validation  
+$result =  validateEmailSmtp($email, $probe_address="", $debug=false);  
+// view results  
+
+echo var_dump($result);;  
+  
+// send email?   
+if ($result) {  
+  //mail(...);  
+}
+
+}
+?>
+
+
+
+<form method="POST">
+<input type="text" name="email">
+<input type="submit" value="submit">
+</form>
+
+
